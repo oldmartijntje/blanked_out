8553ba24cc75cb590798446c8a68c60ad02a3071
Martijn <oldmartijntje@gmail.com>
2025-02-05 16:43:33 +0100

    test

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import { Vector2 } from "./src/system/Vector2.js";
 import { GameLoop } from "./src/system/GameLoop.js";
-import { Main } from './src/objects/Main/Main.js';
+import { Main } from "./src/objects/Main/Main.js";
 import { BaseLevel } from "./src/levels/BaseLevel.js";
 
-const canvas = document.querySelector('#game-canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("#game-canvas");
+const ctx = canvas.getContext("2d");
 
 // In the future, move this into a level object.
 const mainScene = new Main({
@@ -15,17 +15,16 @@ mainScene.setLevel(new BaseLevel());
 const update = (deltaTime) => {
     mainScene.stepEntry(deltaTime, mainScene);
     mainScene.input?.update();
-}
+};
 
 const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     mainScene.drawBackground(ctx);
 
     // save current state (for camera offset)
     ctx.save();
 
-    //offset by camera position
+    // Offset by camera position
     if (mainScene.camera) {
         ctx.translate(mainScene.camera.position.x, mainScene.camera.position.y);
     }
@@ -35,11 +34,39 @@ const draw = () => {
     // restore to original state
     ctx.restore();
 
-    // draw anything above the game world
+    // Draw anything above the game world
     mainScene.drawForeground(ctx);
-}
-
+};
 
 const gameLoop = new GameLoop(update, draw);
-
 gameLoop.start();
+
+// Handle mouse drag movement
+let isDragging = false;
+let lastX = 0, lastY = 0;
+
+const onMouseDown = (event) => {
+    isDragging = true;
+    lastX = event.clientX;
+    lastY = event.clientY;
+};
+
+const onMouseMove = (event) => {
+    if (isDragging) {
+        const deltaX = event.clientX - lastX;
+        const deltaY = event.clientY - lastY;
+        mainScene.camera.position.x += deltaX;
+        mainScene.camera.position.y += deltaY;
+        lastX = event.clientX;
+        lastY = event.clientY;
+    }
+};
+
+const onMouseUp = () => {
+    isDragging = false;
+};
+
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mousemove", onMouseMove);
+canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("mouseleave", onMouseUp);
