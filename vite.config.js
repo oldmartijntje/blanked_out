fe396e995f20caa4337bd422fb43e2f2a018c259
Martijn <oldmartijntje@gmail.com>
2025-02-06 15:20:47 +0100

    nope didn't work

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [],
-    optimizeDeps: {
-        include: ["mqtt"],
-    },
     base: '/blanked_out/',
     build: {
         outDir: 'dist',
-        emptyOutDir: true,
-        rollupOptions: {
-            output: {
-                format: "es",
-            },
-        }
+        emptyOutDir: true
     },
 })
